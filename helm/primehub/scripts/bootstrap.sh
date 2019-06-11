01d5bdda84861eac6cf1639eac75854d30408744
popcorny <celu@infuseai.io>
2019-06-11 15:21:35 +0800

    use service account to run bootstrap.sh

--- helm/primehub/scripts/bootstrap.sh
+++ helm/primehub/scripts/bootstrap.sh
@@ -1,5 +1,21 @@
+set -e
+
 KCADM=keycloak/bin/kcadm.sh
 KC_ADD_USER=keycloak/bin/add-user-keycloak.sh
+
+
+function kc_service_account_add_client_role() {
+  local realm=$1
+  local service_account=$2
+  local client=$3
+  local role=$4
+
+  $KCADM add-roles \
+      -r $realm \
+      --uusername service-account-${service_account} \
+      --cclientid $client \
+      --rolename $role
+}
 
 # FIXME: this wouldn't work without restart the server
 # CAUTION: keycloak will lost data after helm-upgrade in non-persist deployment
@@ -11,24 +27,36 @@ KC_ADD_USER=keycloak/bin/add-user-keycloak.sh
 #fi
 
 # login
-${KCADM} config credentials --server http://localhost:8080/auth --realm master --user ${ADMIN} --password ${ADMIN_PASSWORD}
 
-echo "create realm if not exists"
-if [ ! "$(${KCADM} get realms | jq "[.[] | select(.realm | contains(\"${REALM}\"))] | length")" == 1 ]; then
-  echo "create realm ${REALM}"
-  ${KCADM} create realms -s realm=${REALM} -s enabled=true
+if [[ -z ${CLIENT_ADMIN_SECRET} ]]; then
+  # install
+  echo "Login by user: $ADMIN"
+  ${KCADM} config credentials --server http://localhost:8080/auth --realm master --user ${ADMIN} --password ${ADMIN_PASSWORD}
+  ${KCADM} create realms -s realm=${REALM} -s enabled=true  
 else
-  echo "found realm ${REALM}"
-fi
+  # upgrade
+  echo "Login by service account: $CLIENT_ADMIN"
+  ${KCADM} config credentials --server http://localhost:8080/auth --realm ${REALM} --client ${CLIENT_ADMIN} --secret ${CLIENT_ADMIN_SECRET}
+fi 
 
 echo "create client if not exists"
 if [ ! "$(${KCADM} get clients -r ${REALM} | jq "[.[] | select(.clientId | contains(\"${CLIENT}\"))] | length")" == 1 ]; then
   echo "create client ${CLIENT}"
-  CLIENT_ID=$(${KCADM} create clients -r ${REALM} -s clientId=${CLIENT} -s "redirectUris+=http://hub.${DOMAIN}/*" -f - --id < ./body.json)
+  CLIENT_ID=$(${KCADM} create clients -r ${REALM} -s clientId=${CLIENT} -s "redirectUris+=http://hub.${DOMAIN}/*" -f - --id < ./client.json)
 else
   echo "found client ${CLIENT}"
   CLIENT_ID=$(${KCADM} get clients -r ${REALM} | jq -r ".[] | select(.clientId | contains(\"${CLIENT}\")) | .id")
 fi
+
+echo "create admin client if not exists"
+if [ ! "$(${KCADM} get clients -r ${REALM} | jq "[.[] | select(.clientId | contains(\"${CLIENT_ADMIN}\"))] | length")" == 1 ]; then
+  echo "create client ${CLIENT_ADMIN}"
+  CLIENT_ADMIN_ID=$(${KCADM} create clients -r ${REALM} -s clientId=${CLIENT_ADMIN} -f - --id < ./client-admin.json)
+else
+  echo "found client ${CLIENT_ADMIN}"
+  CLIENT_ADMIN_ID=$(${KCADM} get clients -r ${REALM} | jq -r ".[] | select(.clientId | contains(\"${CLIENT_ADMIN}\")) | .id")
+fi
+kc_service_account_add_client_role ${REALM} ${CLIENT_ADMIN} realm-management realm-admin
 
 echo "create user if not exists"
 if [ ! "$(${KCADM} get users -r ${REALM} | jq "[.[] | select(.username | contains(\"${USER}\"))] | length")" == 1 ]; then
@@ -41,4 +69,6 @@ fi
 
 
 CLIENT_SECRET=$(${KCADM} get clients/${CLIENT_ID}/client-secret -r ${REALM} | jq -r .value)
+CLIENT_ADMIN_SECRET=$(${KCADM} get clients/${CLIENT_ADMIN_ID}/client-secret -r ${REALM} | jq -r .value)
 echo ${CLIENT_SECRET} > client.secret
+echo ${CLIENT_ADMIN_SECRET} > client-admin.secret
