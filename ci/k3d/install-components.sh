f1683b7dbf66bdc44a4db6a742034581c450b382
Timothy Lee <ctiml@infuseai.io>
2020-07-15 15:09:30 +0800

    Add metacontroller as subchart
    Signed-off-by: Timothy Lee <ctiml@infuseai.io>
    Co-authored-by: Ash Wu <hsatac@infuseai.io>

--- ci/k3d/install-components.sh
+++ ci/k3d/install-components.sh
@@ -14,14 +14,6 @@ export GRAPHQL_SECRET_KEY=$(openssl rand -hex 32)
 export HUB_AUTH_STATE_CRYPTO_KEY=$(openssl rand -hex 32)
 export HUB_PROXY_SECRET_TOKEN=$(openssl rand -hex 32)
 
-echo "apply metacontroller yaml"
-# Create metacontroller namespace.
-kubectl create namespace metacontroller
-# Create metacontroller service account and role/binding.
-kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/metacontroller/master/manifests/metacontroller-rbac.yaml
-# Create CRDs for Metacontroller APIs, and the Metacontroller StatefulSet.
-kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/metacontroller/master/manifests/metacontroller.yaml
-
 cd ../chart
 helm dependency update
 helm dependency list
