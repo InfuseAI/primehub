26c769fa51a17d690b1ae6e0d1aabdae2b59ba2b
Timothy Lee <ctiml@infuseai.io>
2020-10-27 12:01:57 +0800

    Sync run-job.sh to add GRANT_SUDO feature
    Signed-off-by: Timothy Lee <ctiml@infuseai.io>

--- chart/scripts/controller/job/run-job.sh
+++ chart/scripts/controller/job/run-job.sh
@@ -8,7 +8,7 @@ PHJOB_NAME=${PHJOB_NAME:-job-$(date '+%Y%m%d%H%M%S')}
 PHJOB_ARTIFACT_ENABLED=${PHJOB_ARTIFACT_ENABLED:-false}
 PHJOB_ARTIFACT_LIMIT_SIZE_MB=${PHJOB_ARTIFACT_LIMIT_SIZE_MB:-100}
 PHJOB_ARTIFACT_LIMIT_FILES=${PHJOB_ARTIFACT_LIMIT_FILES:-1000}
-PHJOB_ARTIFACT_RETENTION_SECONDS=${PHJOB_ARTIFACT_RETENTION_SECONDS:-604800}
+GRANT_SUDO=${GRANT_SUDO:-true}
 
 COMMAND=$1
 
@@ -17,12 +17,11 @@ COMMAND=$1
 # To test artifact copy. We can use
 # PHJOB_ARTIFACT_ENABLED=true ARTIFACTS_SRC='/tmp/artifacts/src' ARTIFACTS_DEST="/tmp/artifacts/$(date '+%Y%m%d-%H%M%S')" ./run-job.sh "echo hello"
 copy_artifacts() {
-  local ARTIFACTS_DRYRUN=${ARTIFACTS_DRYRUN:-false}
-  local ARTIFACTS_SRC=${ARTIFACTS_SRC:-"artifacts"}
-  local ARTIFACTS_DEST=${ARTIFACTS_DEST:-"/phfs/jobArtifacts/${PHJOB_NAME}"}
-  local FILE_COUNT_MAX=$PHJOB_ARTIFACT_LIMIT_FILES
-  local TOTAL_SIZE_MAX=$PHJOB_ARTIFACT_LIMIT_SIZE_MB
-  local RETENTION=$PHJOB_ARTIFACT_RETENTION_SECONDS
+  ARTIFACTS_DRYRUN=${ARTIFACTS_DRYRUN:-false}
+  ARTIFACTS_SRC=${ARTIFACTS_SRC:-"artifacts"}
+  ARTIFACTS_DEST=${ARTIFACTS_DEST:-"/phfs/jobArtifacts/${PHJOB_NAME}"}
+  FILE_COUNT_MAX=$PHJOB_ARTIFACT_LIMIT_FILES
+  TOTAL_SIZE_MAX=$PHJOB_ARTIFACT_LIMIT_SIZE_MB
 
   if [[ ! -e ${ARTIFACTS_SRC} ]]; then
     echo "Artifacts: no artifact found"
@@ -68,18 +67,25 @@ copy_artifacts() {
     echo "Copy artifacts from ${ARTIFACTS_SRC} to ${ARTIFACTS_DEST}"
   fi
 
-  # Metadata
-  mkdir -p "${ARTIFACTS_DEST}/.metadata"
-  if (( $RETENTION > 0 )); then
-    echo "$(($(date +%s) + $RETENTION))" >> "${ARTIFACTS_DEST}/.metadata/expiredAt"
-  fi
-
-  # Prompt information
-  echo "Artifacts: ($((FILE_COUNT)) files / $((TOTAL_SIZE)) MB) uploaded"
+  echo "Artifacts: ($((FILE_COUNT)) files / $((TOTAL_SIZE)) bytes) uploaded"
 }
 
+# Grant sudo
+if [[ "${GRANT_SUDO}" == "true" ]]; then
+  if [[ -n $NB_USER ]]; then
+    USER=$NB_USER
+  fi
+  if [[ -n $USER ]] && [[ -d /etc/sudoers.d ]]; then
+    echo "$USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/job
+  fi
+fi
+
 # Run Command
-bash -c "$COMMAND"
+if command -v sudo > /dev/null && [[ "$GRANT_SUDO" == "true" ]] && [[ -n $USER ]]; then
+  exec sudo -E -H -u $USER PATH=$PATH bash -c "$COMMAND"
+else
+  bash -c "$COMMAND"
+fi
 RETCODE=$?
 
 # Copy Artifacts
