7883520a7332d972db663b27d0d79f5e38519f09
Ash Wu <hSATAC@infuseai.io>
2021-04-15 17:39:18 +0800

    Fix microk8s.crt
    Signed-off-by: Ash Wu <hSATAC@infuseai.io>

--- ci/vm/clean_up.sh
+++ ci/vm/clean_up.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Remove microk8s.containerd to avoid failed to reserve sandbox name issue
-microk8s.stop
-sudo rm -rf /var/snap/microk8s/common/var/lib/containerd
-
-# microk8s.stop disabled services, we need to enable it back
-sudo systemctl list-unit-files | grep microk8s | awk '{print $1}' | xargs sudo systemctl enable
-
 # Remove cloud-init to speed up the boot in virtualbox
 sudo touch /etc/cloud/cloud-init.disabled
 sudo apt remove cloud-init --yes
