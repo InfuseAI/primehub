fb2e0caab30d8d55b66eb84ee07b132a86fd510f
Ash Wu <hSATAC@infuseai.io>
2021-04-15 11:32:34 +0800

    Fix enable microk8s service commands
    Signed-off-by: Ash Wu <hSATAC@infuseai.io>

--- ci/vm/clean_up.sh
+++ ci/vm/clean_up.sh
@@ -5,7 +5,7 @@ microk8s.stop
 sudo rm -rf /var/snap/microk8s/common/var/lib/containerd
 
 # microk8s.stop disabled services, we need to enable it back
-sudo systemctl list-unit-files | grep microk8s | awk '{sudo systemctl enable $1;}'
+sudo systemctl list-unit-files | grep microk8s | awk '{print $1}' | xargs sudo systemctl enable
 
 # Remove cloud-init to speed up the boot in virtualbox
 sudo touch /etc/cloud/cloud-init.disabled
