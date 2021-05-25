3e35dcaca19e858ba59de2087fe37a831fb66252
ericy <ericy@infuseai.io>
2021-05-25 10:56:11 +0800

    change job url matching and increase timeout
    Signed-off-by: ericy <ericy@infuseai.io>

--- tests/features/stepDefinitions/models.js
+++ tests/features/stepDefinitions/models.js
@@ -21,11 +21,11 @@ defineStep("I go to the deployment detail page with name {string}", async functi
     throw new Error(`failed to go to deployment detail page with name ${name}`);
 });
 
-defineStep("I wait for attribute {string} with value {string}", {timeout: 320 * 1000}, async function(att, value) {
+defineStep("I wait for attribute {string} with value {string}", {timeout: 360 * 1000}, async function(att, value) {
     let ele, text, xpath = `//div[text()='${att}']/following-sibling::div`;
     if (att === 'Status') xpath += '//strong';
 
-    for (retryCount=0; retryCount < 20; retryCount++) {
+    for (retryCount=0; retryCount < 35; retryCount++) {
         try {
             [ele] = await this.page.$x(xpath);
             text = await (await ele.getProperty('textContent')).jsonValue();
@@ -34,7 +34,7 @@ defineStep("I wait for attribute {string} with value {string}", {timeout: 320 * 
         console.log(`${att}: ${text}`);
         if (text != value) {
             await this.takeScreenshot(`wait-for-${att}-${value}`);
-            await this.page.waitForTimeout(15000);
+            await this.page.waitForTimeout(10000);
         }
         else {
             return;
