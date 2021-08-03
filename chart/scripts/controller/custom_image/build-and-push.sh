297ad30851cf6ffc1bdf0ba8ef07bf0cab8cddb9
Timothy Lee <ctiml@infuseai.io>
2021-08-03 17:30:51 +0800

    Update image builder script
    Signed-off-by: Timothy Lee <ctiml@infuseai.io>

--- chart/scripts/controller/custom_image/build-and-push.sh
+++ chart/scripts/controller/custom_image/build-and-push.sh
@@ -40,7 +40,11 @@ function build() {
   fi
   echo
   echo "Building image: $TARGET_IMAGE"
-  buildah --storage-driver overlay bud -f /Dockerfile -t $TARGET_IMAGE .
+  if [ "$SKIP_TLS_VERIFY" == "true" ]; then
+    buildah --storage-driver overlay bud --tls-verify=false -f /Dockerfile -t $TARGET_IMAGE .
+  else
+    buildah --storage-driver overlay bud -f /Dockerfile -t $TARGET_IMAGE .
+  fi
   [[ $? -ne 0 ]] && { exit $BUILD_FAILED; }
   echo
 }
