7b7b5af6c5fd894ef0fedc1ec6aa93941cc2ca58
Timothy Lee <ctiml@infuseai.io>
2022-12-07 16:37:26 +0800

    Avoid ulimit command failure
    Signed-off-by: Timothy Lee <ctiml@infuseai.io>

--- chart/scripts/start-notebook.d/ulimit.sh
+++ chart/scripts/start-notebook.d/ulimit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ulimit -n 65535 # Maximum number of open file descriptors
-ulimit -u 65535 # Maximum number of processes available to a single user
+ulimit -n 65535 || true # Maximum number of open file descriptors
+ulimit -u 65535 || true # Maximum number of processes available to a single user
